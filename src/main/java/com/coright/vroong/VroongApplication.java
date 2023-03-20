cb1670f13db430f3772969d16f334ec7b49c317d
cororing <corock.dev@gmail.com>
2023-03-20 19:42:13 +0900

    chore: 스프링 부트 애플리케이션 생성

---
+++ src/main/java/com/coright/vroong/VroongApplication.java
@@ -0,0 +1,13 @@
+package com.coright.vroong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VroongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coright/vroong/VroongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coright.vroong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
